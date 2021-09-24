72cc4b0c8774abe1fe67dca0aa2f51bcf94a677b
Aditya Pundir <adityapundir2k@gmail.com>
2021-09-24 11:26:29 +0530

    Deploy error

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Rap from "./Components/Rap";
 function App() {
   const trackingId = "G-726VB2HM34";
   ReactGA.initialize(trackingId);
-  ReactGA.set({
-    userId: auth.currentUserId(),
-  });
 
   return <Rap />;
 }
